6eb94fc109417e33d0fd0412f2ec6211124a9bb1
Zbalah Addem <addem.zbalah@etu.unilim.fr>
2026-05-05 16:22:23 +0200

    normalement tous les deux a jours

--- frontend/templates/QCM/script.js
+++ frontend/templates/QCM/script.js
@@ -1,22 +1,63 @@
-const templateFile = await fetch("./frontend/templates/QCM/qcm-template.html");
-const template = await templateFile.text();
+import { API } from "../../lib/api.js";
 
-const templateFile2 = await fetch(
-  "./frontend/templates/QCM/qcm-reponse-template.html",
-);
-const templaterep = await templateFile2.text();
+const template = await fetch("/Quiz/frontend/templates/QCM/qcm-template.html")
+  .then((response) => {
+    if (!response.ok) throw new Error("");
+    return response.text();
+  })
+  .catch((err) => {
+    return "";
+  });
 
-const QCM = {};
+const repTemplate = await fetch("/Quiz/frontend/templates/QCM/qcm-reponse-template.html")
+  .then((response) => {
+    if (!response.ok) throw new Error("");
+    return response.text();
+  })
+  .catch((err) => {
+    return "";
+  });
 
-QCM.format = function () {
-  let html = templaterep;
-};
+const QCM = {
+  /**
+  * @param {string|number} questionId
+  */
+  async render(questionId) {
+    const container = document.querySelector("#content");
+    if (!container) {
+      return;
+    }
 
-QCM.formatMany = function (data) {
-  let html = template;
-  let reponsesList = "";
-  for (let reponse of data.reponses) {
-  }
+    const questionsData = await API.getAllQuestions();
+
+    const found = questionsData.questions?.find((q) => Number(q.ordre) === Number(questionId));
+
+    if (!found) return;
+
+    const questionIdResolved = found.id;
+
+    const questionData = await API.getQuestion(questionIdResolved);
+    const reponsesData = await API.getAnswers(questionIdResolved);
+
+    const question = questionData.question ?? questionData;
+    let reponsesHtml = "";
+
+    if (Array.isArray(reponsesData.reponses)) {
+      for (const reponse of reponsesData.reponses) {
+        reponsesHtml += repTemplate.replaceAll("{{reponse}}", String(reponse.intitule ?? ""));
+      }
+    }
+
+    const html = template
+      .replaceAll("{{ordre}}", String(question.ordre ?? "N/A"))
+      .replaceAll("{{total}}", String(Array.isArray(questionsData.questions) ? questionsData.questions.length : 0))
+      .replaceAll("{{question}}", String(question.intitule ?? "Pas de question"))
+      .replaceAll("{{indice}}", String(question.indice ?? ""))
+      .replaceAll("{{reponses}}", reponsesHtml)
+      .replaceAll("{{action}}", "Valider");
+
+    container.innerHTML = html;
+  },
 };
 
 export { QCM };
